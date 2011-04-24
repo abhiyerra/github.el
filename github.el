d591aff80ef71f45825a09e6e59b7d3af725e982
Abhi Yerra <abhi@berkeley.edu>
2011-04-24 01:01:47 -0700

    Added a way to post title + body to github issues.

--- github.el
+++ github.el
@@ -37,20 +37,90 @@
 ;;   (interactive)
 ;;   (github-api-request "GET" (concat "repos/show/" github-login) ""))
 
-; (switch-to-buffer (github-api-request "GET" (concat "repos/show/" github-login) ""))
+;; (switch-to-buffer (github-api-request "GET" (concat "repos/show/" github-login) ""))
+
+(defun github-repos-list ()
+  "List all the user's repos"
+  nil)
+
+(defun github-issues-list ()
+  "List all the issues for a repository"
+  nil)
 
 
-;; TODO: Be able to add body.
-(defun github-issue-new ()
-  "List issues"
+;; This should open a new window. So user can enter title an body.
+;; (defun github-issues-new ()
+;;   "Create a new issue for a github repository"
+;;   (interactive)
+;;   (github-api-request
+;;    "POST"
+;;    (concat "issues/open/"
+;;            (completing-read "Repository: " github-autocomplete-repos nil nil (concat github-login "/")))
+;;    (concat "title="
+;;            (url-hexify-string (read-string "Ticket Title: ")))))
+
+;; Should be based on markdown-mode
+(define-derived-mode github-issues-new-mode fundamental-mode
+  "github-issues-new-mode"
+  "Major mode for entering new Github issues."
+  (org-set-local
+   'header-line-format
+   "github issues ticket. Finish `C-c C-c', abort `C-c C-k'.")
+
+  (define-key github-issues-new-mode-map "\C-c\C-c" 'github-issues-new-create)
+  (define-key github-issues-new-mode-map "\C-c\C-k" 'github-issues-new-cancel))
+
+
+(defun github-issues-new ()
+  "Open a window to enter a new issue."
   (interactive)
-  (github-api-request
-   "POST"
-   (concat "issues/open/"
-           (completing-read "Repository: " github-autocomplete-repos nil nil (concat github-login "/")))
-   (concat "title="
-           (url-hexify-string (read-string "Ticket Title: ")))))
+  (save-excursion
+    (let ((buf (get-buffer-create "*New Github Issue*")))
+      (with-current-buffer buf
+        (switch-to-buffer buf)
+        (github-issues-new-mode)))))
 
+
+(defun github-issues-new-create ()
+  "Create the issue on github."
+  (interactive)
+  (point-min)
+  (let ((title
+         (buffer-substring-no-properties
+          (line-beginning-position)
+          (line-end-position))))
+    (forward-line 1)
+    (github-api-request
+     "POST"
+     (concat "issues/open/"
+             (completing-read "Repository: " github-autocomplete-repos nil nil (concat github-login "/")))
+     (concat "title=" (url-hexify-string title)
+             "&body=" (url-hexify-string
+                       (buffer-substring-no-properties
+                        (line-beginning-position)
+                        (buffer-end 1)))))
+    (kill-buffer)))
+
+
+(defun github-issues-new-cancel ()
+  "Cancel the creation of the the issue."
+  (interactive)
+  (kill-buffer))
+
+;; (defun github-issues-issue ()
+;;   "Show an individual issue from repo's issues."
+;;   nil)
+
+;; (defun github-issues-issue-comment-new ()
+;;   "Add a comment to an issue"
+;;   nil)
+
+;; (defun github-issues-issue-comment-delete ()
+;;   "Add a comment to an issue"
+;;   nil)
+
+
+;; The main way to make a request to github.
 (defun github-api-request (method url params)
   "Make a call to github"
   (let ((url-request-method method)
@@ -63,9 +133,6 @@
     (url-retrieve-synchronously
      (concat "http://github.com/api/v2/json/" url))))
 
-;; TODO: Be able to list issues
-;; TODO: Be able to comment on issues
-;; TODO: Be able to close issues
 
 
 (provide 'github)
